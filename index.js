7cb99d59a15db2b529f7165f799b7b3ea057f4ec
CHRISTIAN EZEANYAECHE <106704559+chisomchris@users.noreply.github.com>
2022-10-27 16:52:21 +0000

    added max level

--- index.js
+++ index.js
@@ -6,67 +6,83 @@ const prompt = (query) => new Promise((resolve) => rl.question(`${query} >>> `, 
 
 // declare Player object
 const player = {
-        level: 1,
-        name: '',
-        isGameOn : true
-    }
+  level: 1,
+  name: '',
+  isGameOn: true,
+  score : 0,
+  maxLevel: false
+}
 // promise based random number generator
-function randonNum(range) { 
-     return Promise.resolve(Math.floor(Math.random() * range) + 1)
+function randonNum(range) {
+  return Promise.resolve(Math.floor(Math.random() * range) + 1)
 }
 
-async function guess(max = 2){
-    let isCorrect = false
-    const correctNum = await randonNum(max)
+async function guess(max = 2) {
+  let isCorrect = false
+  const correctNum = await randonNum(max)
 
-    while(!isCorrect){
-        const input = await prompt(`please enter your guess between 1 - ${max}`)
-        const playerGuess = parseInt(input)
+  while (!isCorrect) {
+    
+    const input = await prompt(`please enter your guess between 1 - ${max}`)
+    const playerGuess = parseInt(input)
 
-        // check for Non Numeric inputs
-        if(Number.isNaN( playerGuess) ){
-            if(/exit/i.test(input)){
-                return player.isGameOn = false
-            }
-            continue
-        }
-        // check if guess was correct
-        if(playerGuess === correctNum){
-            isCorrect = true
-            player.level = player.level + 1
-            console.log(`\n${player.name}, you got the number correct, the correct number is ${correctNum}`)
-            return;
-        }
-        console.log(`${player.name}, Your guess was wrong, please try again...`)
-        }
+    // check for Non Numeric inputs
+    if (Number.isNaN(playerGuess)) {
+      if (/exit/i.test(input)) {
+        return player.isGameOn = false
+      }
+      continue
+    }
+    // check if guess was correct
+    if (playerGuess === correctNum) {
+      isCorrect = true
+      player.level = player.level + 1
+      player.score = player.score + 5
+      if(player.level === 10){
+      player.maxLevel = true
+      return player.isGameOn = false
+  }
+      console.log(`\n${player.name}, you got the number correct, the correct number is ${correctNum}`)
+      return;
+    }
+    player.score = player.score - 0.5
+    console.log(`${player.name}, Your guess was wrong, please try again...`)
+  }
+}
+
+// IIFE -> Run immediately on execution
+(async () => {
+  try {
+    console.log('Enter "exit" in the prompt or use Ctrl + C, to quit\n')
+
+    // prompt user for input
+    const userName = await prompt('please enter your name: ')
+    const user = /\S/.test(userName) ? userName : "Player"
+
+    // set player name
+    player.name = user
+
+    console.log(`\nWelcome ${player.name}!`)
+
+    while (player.isGameOn) {
+      console.log(`\n${player.name}, you are playing level ${player.level}\n`)
+      await guess(player.level + 1)
     }
 
-    // IIFE -> Run immediately on execution
-(async()=>{
-    try {
-        console.log('Enter "exit" in the prompt or use Ctrl + C, to quit\n')
-
-        // prompt user for input
-        const userName = await prompt('please enter your name: ')
-        const user = /\S/.test(userName) ? userName : "Player"
-
-        // set player name
-        player.name = user
-
-        while( player.isGameOn ) {
-            console.log(`\n${player.name}, you are playing level ${player.level}`)
-            await guess(player.level + 1)
-        }
-       
-        rl.close()
-    } catch (error) {
-        console.error("Unable to prompt", error);
-    }
+    rl.close()
+  } catch (error) {
+    console.error("Unable to prompt", error);
+  }
 })()
 
 // 
-rl.on('close', () =>{ 
-    console.log(`\nGame Over\n${player.name}, you reached level ${player.level}`)
-    process.exit(0)
-    }
+rl.on('close', () => {
+if (player.maxLevel) {
+  
+console.log(`\nCongratulation ${player.name},\nyou completed all levels and got ${Math.round(player.score)} points\n`)
+} else {
+console.log(`\nGame Over\n${player.name}, you reached level ${player.level} and got ${Math.round(player.score)} points\n`)
+}
+  process.exit(0)
+}
 );
